Fix alpha sign; Add num_z=0; Track train losses

VAE_train.py
@@ -14,129 +14,173 @@ from disvae.models.vae import VAE
 from disvae.models.losses import BtcvaeLoss
 
 
-def objective(trial):
-    #model hyperparams
-    conv_depth = trial.suggest_int('Dꟲ', 2, 4)
-    conv_width = trial.suggest_int('Wꟲ', 4, 9)  # number of channels
-    mlp_depth = trial.suggest_int('Dᴹ', 2, 5)
-    mlp_width = 2 ** trial.suggest_int('㏒₂Wᴹ', 4, 9)
-    bn_mode = trial.suggest_categorical('BA', ['BA', 'A', 'AB'])
+class Objective:
+    def __init__(self, num_z, P_shape, num_p, t_dataset, v_dataset, num_epoch, device,
+                 study_path):
+        self.num_z = num_z
+        self.P_shape = P_shape
+        self.num_p = num_p
+        self.t_dataset = t_dataset
+        self.v_dataset = v_dataset
+        self.num_epoch = num_epoch
+        self.device = device
+        self.study_path = study_path
 
-    #loss hyperparams
-    lamda = trial.suggest_float('λ', 0.01, 10, log=True)
-    alpha = - trial.suggest_float('-α', 1, 100, log=True)
-    beta = trial.suggest_float('β', 1, 100, log=True)
-    gamma = trial.suggest_float('γ', 0.1, 10, log=True)
+    def __call__(self, trial):
+        #model hyperparams
+        conv_depth = trial.suggest_int('Dꟲ', 2, 4)
+        conv_width = trial.suggest_int('Wꟲ', 4, 9)  # number of channels
+        mlp_depth = trial.suggest_int('Dᴹ', 2, 5)
+        mlp_width = 2 ** trial.suggest_int('㏒₂Wᴹ', 4, 9)
+        bn_mode = trial.suggest_categorical('BA', ['BA', 'A', 'AB'])
 
-    #training hyperparams
-    lr = trial.suggest_float('lr', 1e-6, 1e-2, log=True)
-    b1 = 1 - trial.suggest_float('1-β₁', 0.1, 0.5, log=True)
-    b2 = 1 - trial.suggest_float('1-β₂', 1e-3, 0.1, log=True)
-    wd = trial.suggest_float('wd', 1e-6, 1e-3, log=True)
-    batch_size = 2 ** trial.suggest_int('㏒₂B', 6, 9)
+        #loss hyperparams
+        lamda = alpha = beta = gamma = 1
+        if self.num_z >= 1:
+            lamda = trial.suggest_float('λ', 0.01, 10, log=True)
+            alpha = trial.suggest_float('α', 0.1, 10, log=True)
+            beta = trial.suggest_float('β', 1, 100, log=True)
+            gamma = trial.suggest_float('γ', 0.1, 10, log=True)
 
-    t_loader = DataLoader(t_dataset, batch_size=batch_size, shuffle=True)
-    v_loader = DataLoader(v_dataset, batch_size=len(v_dataset), shuffle=False)
-    model = VAE(num_z, P_shape, num_p,
-                conv_depth, conv_width, mlp_depth, mlp_width,
-                bn_mode).to(device, non_blocking=True)
-    criterion = BtcvaeLoss(len(t_dataset), lamda=lamda, alpha=alpha, beta=beta,
-                           gamma=gamma)
-    assert len(t_dataset) == len(v_dataset), 'otherwise we need two criteria'
-    optimizer = AdamW(model.parameters(), lr=lr, betas=(b1, b2), weight_decay=wd)
-    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, threshold=0.01)
+        #training hyperparams
+        lr = trial.suggest_float('lr', 1e-6, 1e-1, log=True)
+        b1 = 1 - trial.suggest_float('1-β₁', 0.1, 0.5, log=True)
+        b2 = 1 - trial.suggest_float('1-β₂', 1e-3, 0.1, log=True)
+        wd = trial.suggest_float('wd', 1e-6, 1e-3, log=True)
+        batch_size = 2 ** trial.suggest_int('㏒₂B', 6, 9)
 
-    min_train, min_valid = math.inf, math.inf
-    min_rec, max_mi, min_tc, min_dwkl = math.inf, -math.inf, math.inf, math.inf
-    min_kl = math.inf
-    for epoch in range(1000):
-        if optimizer.param_groups[0]['lr'] <= 1e-8:
-            break
+        t_loader = DataLoader(self.t_dataset, batch_size=batch_size, shuffle=True)
+        v_loader = DataLoader(self.v_dataset, batch_size=len(self.v_dataset),
+                              shuffle=False)
+        model = VAE(self.num_z, self.P_shape, self.num_p,
+                    conv_depth, conv_width, mlp_depth, mlp_width,
+                    bn_mode).to(self.device, non_blocking=True)
+        criterion = BtcvaeLoss(len(self.t_dataset), lamda=lamda, alpha=alpha, beta=beta,
+                               gamma=gamma)
+        assert len(self.t_dataset) == len(self.v_dataset), 'otherwise need 2 criteria'
+        optimizer = AdamW(model.parameters(), lr=lr, betas=(b1, b2), weight_decay=wd)
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=10,
+                                                   threshold=0.01)
 
-        model.train()
-        storer = {k: torch.zeros(1, device=device) for k in
-                  ['loss', 'rec', 'mi', 'tc', 'dwkl', 'kl']
-                  + [f'kl_{d}' for d in range(num_z)]}
-        for P, p in t_loader:
-            Prat = P[:, 1]
-            loss = criterion(*model(P, p), Prat, storer=storer)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        epoch_loss = storer['loss'].item() / len(t_loader)
-        min_train = min(epoch_loss, min_train)
+        t_loss, v_loss = math.inf, math.inf
+        t_rec, v_rec = math.inf, math.inf
+        t_mi, v_mi = math.inf, math.inf
+        t_tc, v_tc = math.inf, math.inf
+        t_dwkl, v_dwkl = math.inf, math.inf
+        t_kl, v_kl = math.inf, math.inf
+        for epoch in range(self.num_epoch):
+            if optimizer.param_groups[0]['lr'] <= 1e-8:
+                break
 
-        model.eval()
-        storer = {k: torch.zeros(1, device=device) for k in
-                  ['loss', 'rec', 'mi', 'tc', 'dwkl', 'kl']
-                  + [f'kl_{d}' for d in range(num_z)]}
-        with torch.no_grad():
-            for P, p in v_loader:
+            model.train()
+            storer = {k: torch.zeros(1, device=self.device) for k in
+                    ['loss', 'rec', 'mi', 'tc', 'dwkl', 'kl']
+                    + [f'kl_{d}' for d in range(num_z)]}
+            for P, p in t_loader:
                 Prat = P[:, 1]
                 loss = criterion(*model(P, p), Prat, storer=storer)
-            epoch_loss = storer['loss'].item() / len(v_loader)
-            rec = storer['rec'].item() / len(v_loader)
-            mi = storer['mi'].item() / len(v_loader)
-            tc = storer['tc'].item() / len(v_loader)
-            dwkl = storer['dwkl'].item() / len(v_loader)
-            kl = storer['kl'].item() / len(v_loader)
-            min_valid = min(epoch_loss, min_valid)
-            min_rec = min(rec, min_rec)
-            max_mi = max(mi, max_mi)
-            min_tc = min(tc, min_tc)
-            min_dwkl = min(dwkl, min_dwkl)
-            min_kl = min(kl, min_kl)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            t_loss = min(storer['loss'].item() / len(t_loader), t_loss)
+            t_rec = min(storer['rec'].item() / len(t_loader), t_rec)
+            t_mi = min(storer['mi'].item() / len(t_loader), t_mi)
+            t_tc = min(storer['tc'].item() / len(t_loader), t_tc)
+            t_dwkl = min(storer['dwkl'].item() / len(t_loader), t_dwkl)
+            t_kl = min(storer['kl'].item() / len(t_loader), t_kl)
 
-        scheduler.step(epoch_loss)
+            model.eval()
+            storer = {k: torch.zeros(1, device=self.device) for k in
+                    ['loss', 'rec', 'mi', 'tc', 'dwkl', 'kl']
+                    + [f'kl_{d}' for d in range(num_z)]}
+            with torch.no_grad():
+                for P, p in v_loader:
+                    Prat = P[:, 1]
+                    loss = criterion(*model(P, p), Prat, storer=storer)
+                v_loss = min(storer['loss'].item() / len(v_loader), v_loss)
+                v_rec = min(storer['rec'].item() / len(v_loader), v_rec)
+                v_mi = min(storer['mi'].item() / len(v_loader), v_mi)
+                v_tc = min(storer['tc'].item() / len(v_loader), v_tc)
+                v_dwkl = min(storer['dwkl'].item() / len(v_loader), v_dwkl)
+                v_kl = min(storer['kl'].item() / len(v_loader), v_kl)
 
-    print(f'{trial.number=}, {epoch=}, {min_train=}, {min_valid=}')
-    print(f'  {min_rec=}, {max_mi=}, {min_tc=}, {min_dwkl=} {min_kl=}', flush=True)
+            scheduler.step(v_loss)
 
-    trial.set_user_attr('epoch', epoch)
-    trial.set_user_attr('min_train', min_train)
-    trial.set_user_attr('min_valid', min_valid)
-    trial.set_user_attr('min_rec', min_rec)
-    trial.set_user_attr('max_mi', max_mi)
-    trial.set_user_attr('min_tc', min_tc)
-    trial.set_user_attr('min_dwkl', min_dwkl)
-    trial.set_user_attr('min_kl', min_kl)
+        print(f'{trial.number=}, {epoch=}, {t_loss=:.1e}, {v_loss=:.1e}')
+        print(f'  {t_rec=:.1e}, {t_mi=:.1e}, {t_tc=:.1e}, {t_dwkl=:.1e} {t_kl=:.1e}')
+        print(f'  {v_rec=:.1e}, {v_mi=:.1e}, {v_tc=:.1e}, {v_dwkl=:.1e} {v_kl=:.1e}',
+              flush=True)
 
-    #states = {
-    #    'epoch': epoch,
-    #    'model': model.state_dict(),
-    #    'optimizer': optimizer.state_dict(),
-    #    'scheduler': scheduler.state_dict(),
-    #    'rng': torch.get_rng_state(),
-    #    'min_train': min_train,
-    #    'min_valid': min_valid,
-    #    'min_rec': min_rec,
-    #    'max_mi': max_mi,
-    #    'min_tc': min_tc,
-    #    'min_dwkl': min_dwkl,
-    #    'min_kl': min_kl,
-    #}
-    #torch.save(states, f'{study_path}_{trial.number}.pt')
+        trial.set_user_attr('epoch', epoch)
+        trial.set_user_attr('t_loss', t_loss)
+        trial.set_user_attr('v_loss', v_loss)
+        trial.set_user_attr('t_rec', t_rec)
+        trial.set_user_attr('v_rec', v_rec)
+        trial.set_user_attr('t_mi', t_mi)
+        trial.set_user_attr('v_mi', v_mi)
+        trial.set_user_attr('t_tc', t_tc)
+        trial.set_user_attr('v_tc', v_tc)
+        trial.set_user_attr('t_dwkl', t_dwkl)
+        trial.set_user_attr('v_dwkl', v_dwkl)
+        trial.set_user_attr('t_kl', t_kl)
+        trial.set_user_attr('v_kl', v_kl)
 
-    torch.cuda.empty_cache()
+        #states = {
+        #    'epoch': epoch,
+        #    'model': model.state_dict(),
+        #    'optimizer': optimizer.state_dict(),
+        #    'scheduler': scheduler.state_dict(),
+        #    'rng': torch.get_rng_state(),
+        #    't_loss': t_loss,
+        #    'v_loss': v_loss,
+        #    't_rec': t_rec,
+        #    'v_rec': v_rec,
+        #    't_mi': t_mi,
+        #    'v_mi': v_mi,
+        #    't_tc': t_tc,
+        #    'v_tc': v_tc,
+        #    't_dwkl': t_dwkl,
+        #    'v_dwkl': v_dwkl,
+        #    't_kl': t_kl,
+        #    'v_kl': v_kl,
+        #}
+        #torch.save(states, f'{self.study_path}_{trial.number}.pt')
 
-    return min_rec, max_mi, min_tc
+        torch.cuda.empty_cache()
+
+        if self.num_z == 0:
+            return v_rec
+        return v_rec, v_tc
 
 
-def main():
+def optune(num_z, P_shape, num_p, t_dataset, v_dataset, device):
+    study_path = f"optuna/d{num_z}_{date.today().strftime('%m%d')}"
+    os.makedirs(os.path.dirname(study_path), exist_ok=True)
+
     n_trials = 2**14
     timeout = 6.9 * 24 * 3600
     n_startup_trials = 2**6
 
+    sampler = optuna.samplers.TPESampler(
+        n_startup_trials=n_startup_trials,
+        seed=42,
+        multivariate=True,
+        warn_independent_sampling=True,
+        constant_liar=True,
+    )
+
     study = optuna.create_study(
         storage=f'sqlite:///{study_path}.db',
-        sampler=optuna.samplers.TPESampler(n_startup_trials=n_startup_trials, seed=42),
+        sampler=sampler,
         pruner=None,  # currently not for multi-objective optimization
         load_if_exists=True,
-        directions=['minimize', 'maximize', 'minimize'],
-        #directions=['minimize', 'maximize', 'minimize', 'minimize', 'minimize'],
+        directions=['minimize'] * (1 if num_z == 0 else 2),
     )
-    study.set_metric_names(['rec', 'mi', 'tc'])
-    #study.set_metric_names(['rec', 'mi', 'tc', 'dwkl', 'kl'])
+
+    num_epoch = 1000
+    objective = Objective(num_z, P_shape, num_p, t_dataset, v_dataset, num_epoch,
+                          device, study_path)
+
     study.optimize(objective, n_trials=n_trials, timeout=timeout, gc_after_trial=True)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
@@ -147,42 +191,39 @@ def main():
     print('  Number of pruned trials: ', len(pruned_trials))
     print('  Number of complete trials: ', len(complete_trials))
 
-    # single-objective
-    #print('Best trial:')
-    #trial = study.best_trial
-    #print('  Value: ', trial.value)
-    #print('  Params: ')
-    #for key, value in trial.params.items():
-    #    print('    {}: {}'.format(key, value))
+    if num_z == 0:
+        # single-objective
+        print('Best trial:')
+        trial = study.best_trial
+        print('  Value: ', trial.value)
+        print('  Params: ')
+        for key, value in trial.params.items():
+            print('    {}: {}'.format(key, value))
+    else:
+        # multi-objective
+        print('Pareto front:')
+        trials = sorted(study.best_trials, key=lambda trial: trial.values)
+        for trial in trials:
+            rec, mi, tc = trial.values
+            print(f'  Trial#{trial.number}')
+            print(f'    Values: {rec=}, {mi=}, {tc=}')
+            print(f'    Params: {trial.params}')
 
-    # multi-objective
-    print('Pareto front:')
-    trials = sorted(study.best_trials, key=lambda trial: trial.values)
-    for trial in trials:
-        rec, mi, tc = trial.values
-        #rec, mi, tc, dwkl, kl = trial.values
-        print(f'  Trial#{trial.number}')
-        print(f'    Values: {rec=}, {mi=}, {tc=}')
-        #print(f'    Values: {rec=}, {mi=}, {tc=}, {dwkl=} {kl=}')
-        print(f'    Params: {trial.params}')
-
-    print('Trials:')
-    df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'),
-                                multi_index=True)
-    print(df.to_markdown())
+    #print('Trials:')
+    #df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'),
+    #                            multi_index=True)
+    #print(df.to_markdown())
 
 
 if __name__ == '__main__':
     num_z = int(sys.argv[1])
 
-    study_path = f"optuna/d{num_z}_{date.today().strftime('%m%d')}"
-    os.makedirs(os.path.dirname(study_path), exist_ok=True)
     device = torch.device('cuda', 0)
 
     P_set = np.load('IllustrisTNG_powers.npy')[:, :2]  # shape = (1024, 2, 4, 18)
     P_set = torch.from_numpy(P_set).to(device)
     P_set = torch.log(P_set)
-    P_set[:, 1] -= P_set[:, 0]
+    P_set[:, 1] -= P_set[:, 0]  # log(P_hydro / P_dmo)
     Pt_set, Pv_set = P_set[:512], P_set[512:]
 
     p_set = np.load('IllustrisTNG_params.npy')[:, 0, -1]  # shape = (1024, 3)
@@ -195,4 +236,4 @@ if __name__ == '__main__':
     t_dataset = TensorDataset(Pt_set, pt_set)
     v_dataset = TensorDataset(Pv_set, pv_set)
 
-    main()
+    optune(num_z, P_shape, num_p, t_dataset, v_dataset, device)

disvae/models/vae.py
@@ -48,7 +48,7 @@ def get_block(header, l, in_width, out_width, leaky_slope, bn_mode):
 
 
 class VAE(nn.Module):
-    def __init__(self, num_z=2, P_shape=(2, 4, 18), num_p=3,
+    def __init__(self, num_z=1, P_shape=(2, 4, 18), num_p=3,
                  conv_depth=2, conv_width=4, mlp_depth=3, mlp_width=64,
                  bn_mode='A'):  #FIXME use optuna best hyperparams
         super().__init__()
@@ -66,21 +66,22 @@ class VAE(nn.Module):
 
         leaky_slope = 1 / torch.e
 
-        self.econv = OrderedDict()
-        for l in range(conv_depth):
-            in_chan = two * num_a + num_p if l == 0 else conv_width
-            self.econv.update(get_block('ec', l, in_chan, conv_width, leaky_slope,
-                                        bn_mode))
-        self.econv = nn.Sequential(self.econv)
+        if num_z >= 1:
+            self.econv = OrderedDict()
+            for l in range(conv_depth):
+                in_chan = two * num_a + num_p if l == 0 else conv_width
+                self.econv.update(get_block('ec', l, in_chan, conv_width, leaky_slope,
+                                            bn_mode))
+            self.econv = nn.Sequential(self.econv)
 
-        self.emlp = OrderedDict()
-        for l in range(mlp_depth - 1):
-            in_feat = conv_width * num_k + num_p if l == 0 else mlp_width
-            self.emlp.update(get_block('em', l, in_feat, mlp_width, leaky_slope,
-                                       bn_mode))
-        out_feat = 2 * num_z
-        self.emlp[f'eml{mlp_depth-1}'] = Linear(mlp_width, out_feat, linear=True)
-        self.emlp = nn.Sequential(self.emlp)
+            self.emlp = OrderedDict()
+            for l in range(mlp_depth - 1):
+                in_feat = conv_width * num_k + num_p if l == 0 else mlp_width
+                self.emlp.update(get_block('em', l, in_feat, mlp_width, leaky_slope,
+                                        bn_mode))
+            out_feat = 2 * num_z
+            self.emlp[f'eml{mlp_depth-1}'] = Linear(mlp_width, out_feat, linear=True)
+            self.emlp = nn.Sequential(self.emlp)
 
         self.dmlp = OrderedDict()
         for l in range(mlp_depth):
@@ -122,7 +123,7 @@ class VAE(nn.Module):
         return mean
 
     def decode(self, z, Pdmo, p):
-        x = torch.cat([z, p], dim=1)
+        x = p if z is None else torch.cat([z, p], dim=1)
         x = self.dmlp(x)
 
         x = x.unflatten(1, (self.conv_width, self.num_k))
@@ -132,11 +133,15 @@ class VAE(nn.Module):
         return Prec
 
     def forward(self, P, p):
-        mean, logvar = self.encode(P, p)
-
-        z = self.reparameterize(mean, logvar)
-
         Pdmo = P[:, 0]
+
+        if self.num_z == 0:
+            Prec = self.decode(None, Pdmo, p)
+
+            return None, None, None, Prec
+
+        mean, logvar = self.encode(P, p)
+        z = self.reparameterize(mean, logvar)
         Prec = self.decode(z, Pdmo, p)
 
         return mean, logvar, z, Prec